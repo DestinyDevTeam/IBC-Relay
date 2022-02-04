39e9f46ee09ab5710a4123139b16e424c5fdcfeb
DestinyDevTeam <74211870+DestinyDevTeam@users.noreply.github.com>
2022-02-04 17:23:17 +0100

    Update ecosystem.config.js

--- reporter/ecosystem.config.js
+++ reporter/ecosystem.config.js
@@ -1,17 +1,18 @@
 const ms = require('ms')
 module.exports = {
-  apps: [
-    {
-      name: "ibc-worker",
-      script: "./build/index.js",
-      "instances": "1",
-      "exec_mode": "cluster",
-      env: {
-        PORT: process.env.PORT,
-        TELOS_ENDPOINT: process.env.TELOS_ENDPOINT,
-        TELOS_IBC: process.env.TELOS_IBC,
-        NODE_ENV: process.env.NODE_ENV
-      }
+  apps : [{
+    name   : "ibc-worker",
+    script : "./build/index.js",
+    instances : 1,
+    exec_mode : "cluster"
+    env_production: {
+       PORT: process.env.PORT,
+       TELOS_ENDPOINT: process.env.TELOS_ENDPOINT,
+       TELOS_IBC: process.env.TELOS_IBC,
+       NODE_ENV: process.env.NODE_ENV
     },
-  ]
-};
+    env_development: {
+       NODE_ENV: "development"
+    }
+  }]
+}
